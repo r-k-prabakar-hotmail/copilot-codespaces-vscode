9cae395095e753bb219a0409c6f56a267409fb02
r-k-prabakar-hotmail <r.k.prabakar1987@hotmail.com>
2025-01-23 10:47:35 +0000

    Copilot fifth commit

--- comments.js
+++ comments.js
@@ -1,62 +1,29 @@
-//Create web server using express
+//create web server
 const express = require('express');
+const comments = require('./comments');
 const app = express();
-const fs = require('fs');
+const path = require('path');
 const bodyParser = require('body-parser');
 
-// Set up the server to serve static files from the 'public' directory
-app.use(express.static('public'));
-
-// Set up the server to use the body-parser middleware for parsing JSON
+//parse json
 app.use(bodyParser.json());
 
-// Set up the server to use the body-parser middleware for parsing URL encoded request bodies
-app.use(bodyParser.urlencoded({ extended: true }));
+//static files
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Use the GET method to send the homepage
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
-});
-
-// Use the POST method to log the comment to the console
-app.post('/comment', (req, res) => {
-    // Log the comment to the console
-    console.log(req.body.comment);
-    // Send a response to the client
-    res.send('Comment received');
-    // Write the comment to the comments.txt file
-    fs.appendFile('comments.txt', req.body.comment + '\n', (err) => {
-        if (err) {
-            console.log(err);
-        }
-    });
-});
-
-// Use the GET method to send the comments.txt file
+//get comments
 app.get('/comments', (req, res) => {
-    // Read the comments.txt file
-    fs.readFile('comments.txt', 'utf8', (err, data) => {
-        if (err) {
-            console.log(err);
-        }
-        // Send the comments to the client
-        res.send(data);
-    });
+    res.json(comments.getComments());
 });
 
-// Use the DELETE method to delete the comments.txt file
-app.delete('/comments', (req, res) => {
-    // Delete the comments.txt file
-    fs.unlink('comments.txt', (err) => {
-        if (err) {
-            console.log(err);
-        }
-    });
-    // Send a response to the client
-    res.send('Comments deleted');
+//add comments
+app.post('/comments', (req, res) => {
+    const { author, text } = req.body;
+    comments.addComment(author, text);
+    res.status(201).end();
 });
 
-// Start the server on port 3000
+//start server
 app.listen(3000, () => {
-    console.log('Server started on http://localhost:3000');
+    console.log('Server is running on http://localhost:3000/');
 });
